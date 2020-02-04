20b9def931d51ccedb8189279a55cb03accdba16
takazumi <takazumikwkm@gmail.com>
2020-02-04 17:00:40 +0900

    添削箇所を修正しました。

--- script.js
+++ script.js
@@ -2,15 +2,16 @@
 'use strict'
 
 function fizzbuzz() {
-    for (let i = 0; i <= 100; i++) {
-    if(i % 3 === 0 && i % 5 === 0) {
-        console.log("fFizzBuzz");
-    } else if(i % 3 === 0) {
-        console.log("Fizz");
-    } else if(i % 5 === 0) {
-        console.log("Buzz");
-    } else {console.log(i);
+    for (let i = 1; i <= 100; i++) {
+        if(i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+        } else if(i % 3 === 0) {
+            console.log("Fizz");
+        } else if(i % 5 === 0) {
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
     }
-}
 }
 fizzbuzz();
