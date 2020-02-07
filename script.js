74c178211f8da0f443f90d3c1b821b71f599a3c1
takazumi <takazumikwkm@gmail.com>
2020-02-07 14:52:06 +0900

    ナベアツできました！

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 
 'use strict'
 
-function fizzbuzz() {
+const nabeatsu = () => {
     for (let i = 1; i <= 100; i++) {
-        if(i % 3 === 0 && i % 5 === 0) {
-            console.log("FizzBuzz");
-        } else if(i % 3 === 0) {
-            console.log("Fizz");
-        } else if(i % 5 === 0) {
-            console.log("Buzz");
+        if(i % 3 === 0) {
+            console.log(i + "!");
+        } else if(i.toString().match(/3/)) {
+            console.log(i + "!");
         } else {
             console.log(i);
         }
     }
 }
-fizzbuzz();
+nabeatsu();
