0b8cbb45894775f98a9a9ceff63615a4f76b2922
takazumi <takazumikwkm@gmail.com>
2020-02-07 19:46:16 +0900

    ナベアツ課題

---
+++ n-script.js
@@ -0,0 +1,12 @@
+const nabeatsu = () => {
+    for (let i = 1; i <= 100; i++) {
+        if(i % 3 === 0) {
+            console.log(i + "!");
+        } else if(i.toString().match(/3/)) {
+            console.log(i + "!");
+        } else {
+            console.log(i);
+        }
+    }
+}
+nabeatsu();
